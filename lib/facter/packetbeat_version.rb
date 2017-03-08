45f5bbb04f29c948e3c0c44d90280f04dd3873ad
corey-hammerton <corey.hammerton@gmail.com>
2017-03-08 17:09:10 -0500

    rubocop: Cleaning up the configuration
    Removing all checks that are enabled by default, based on https://github.com/bbatsov/rubocop/blob/v0.47.1/config/enabled.yml.
    Enabled the checks disabled by this configuration, based on https://github.com/bbatsov/rubocop/blob/v0.47.1/config/enabled.yml.
    
    Added a test case in checking for nil output from the packetbeat_version fact.

--- lib/facter/packetbeat_version.rb
+++ lib/facter/packetbeat_version.rb
@@ -3,8 +3,10 @@ require 'facter'
 Facter.add('packetbeat_version') do
   confine :kernel => 'Linux' # rubocop:disable Style/HashSyntax
   setcode do
-    packetbeat_version = Facter::Util::Resolution.exec('/usr/share/packetbeat/bin/packetbeat -version')
+    packetbeat_version = Facter::Util::Resolution.exec(
+      '/usr/share/packetbeat/bin/packetbeat -version'
+    )
 
-    %r{^packetbeat version ([^\s]+)?}.match(packetbeat_version)[1] unless packetbeat_version.nil?
+    %r{/^packetbeat version ([^\s]+)?/}.match(packetbeat_version)[1]
   end
 end
--- spec/unit/packetbeat_version_spec.rb
+++ spec/unit/packetbeat_version_spec.rb
@@ -7,11 +7,28 @@ describe 'packetbeat_version' do
   context 'on Linux OS' do
     before do
       Facter.fact(:kernel).stubs(:value).returns('Linux')
-      Facter::Util::Resolution.stubs(:exec).with('/usr/share/packetbeat/bin/packetbeat -version').returns('packetbeat version 5.2.0 (amd64), libbeat 5.2.0')
+      Facter::Util::Resolution.stubs(:exec).with(
+        '/usr/share/packetbeat/bin/packetbeat -version'
+      ).returns(
+        'packetbeat version 5.2.0 (amd64), libbeat 5.2.0'
+      )
     end
 
     it do
       expect(Facter.fact(:packetbeat_version).value).to eq('5.2.0')
+    end
+  end
+
+  context 'not installed' do
+    before do
+      Facter.fact(:kernel).stubs(:value).returns('Linux')
+      Facter::Util::Resolution.stubs(:exec).with(
+        '/usr/share/packetbeat/bin/packetbeat -version'
+      ).returns(nil)
+    end
+
+    it do
+      expect(Facter.fact(:packetbeat_version).value).to eq(nil)
     end
   end
 end
