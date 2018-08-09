19f1138289ff8104ae5a01c33cc3c0a1cd61694e
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2018-08-09 16:54:02 +0100

    Put conditional around running exec

--- lib/facter/packetbeat_version.rb
+++ lib/facter/packetbeat_version.rb
@@ -3,10 +3,12 @@ require 'facter'
 Facter.add('packetbeat_version') do
   confine :kernel => 'Linux' # rubocop:disable Style/HashSyntax
   setcode do
-    packetbeat_version = Facter::Util::Resolution.exec(
-      '/usr/share/packetbeat/bin/packetbeat -version',
-    )
+    if File.exists? '/usr/share/packetbeat/bin/packetbeat'
+      packetbeat_version = Facter::Util::Resolution.exec(
+        '/usr/share/packetbeat/bin/packetbeat -version',
+      )
 
-    %r{^packetbeat version ([0-9.]+)}.match(packetbeat_version)[1]
+      %r{^packetbeat version ([0-9.]+)}.match(packetbeat_version)[1]
+    end
   end
 end
